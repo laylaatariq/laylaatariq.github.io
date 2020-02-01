a44bb931a89a8ba8696cedef04fe8f8f39f9c7ce
Laylaa Tariq <laaylatariq@gmail.com>
2020-02-01 13:35:04 -0600

    testing

--- Plot/static/js/app.js
+++ Plot/static/js/app.js
@@ -1,6 +1,46 @@
-d3.json("data/samples.json").then((incomingData) => {
-   
-    var data = incomingData;
+d3.json('samples.json').then((incomingData) => {
 
-    console.log(incomingData)
+    var data = incomingData;
+    
+    //Alotting data to the drop down menu
+   var dropdown = d3.select("#selDataset");
+
+    var subjID = data.names;
+
+    // console.log(subjID);
+
+    subjID.forEach((names) => {
+
+        var option = dropdown.append("option");
+        option.text(names);
+    });
+
+    var defaultID = subjID[0];
+    getMetaData(defaultID);
+
+    function getMetaData(ID){
+
+        // var metaData = data.metadata;
+
+        // var filteredData = {};
+
+        // for (let i = 0; i<metaData.length;i++) {
+
+        //     if(metaData[i].id === ID){
+
+        //         filteredData = dataID[i];
+        //         break;
+        //     }
+        // }
+
+        // console.log(filteredData);
+
+        // filteredData.forEach((pair) =>{
+
+        //     Object.entries(pair).forEach(function([key, value]){
+        //         console.log(key, value);
+        //     })
+        // })
+    }
+   function optionChanged(ID){}
 });
