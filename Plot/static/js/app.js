d333b225c621b2d76e3c119a9649e9dae07d065d
Laylaa Tariq <laaylatariq@gmail.com>
2020-02-04 10:18:52 -0600

    almost done with project just the bonus part left, will do it tomorrow

--- Plot/static/js/app.js
+++ Plot/static/js/app.js
@@ -7,40 +7,117 @@ d3.json('samples.json').then((incomingData) => {
 
     var subjID = data.names;
 
-    // console.log(subjID);
-
     subjID.forEach((names) => {
 
         var option = dropdown.append("option");
         option.text(names);
     });
 
+    //Setting Default screen ouptut
     var defaultID = subjID[0];
     getMetaData(defaultID);
+    plots(defaultID);
 
+    //Function to get the data for the respective ID
     function getMetaData(ID){
 
-        // var metaData = data.metadata;
+        var metaData = data.metadata;
 
-        // var filteredData = {};
+        var filteredData = {};
 
-        // for (let i = 0; i<metaData.length;i++) {
+        //For loop to go over the array to get the data for the corresponding ID
+        for (let i = 0; i<metaData.length;i++) {
 
-        //     if(metaData[i].id === ID){
+            if(metaData[i].id == ID){
+                filteredData = metaData[i];
+                break;
+            }
+        }
 
-        //         filteredData = dataID[i];
-        //         break;
-        //     }
-        // }
-
-        // console.log(filteredData);
-
-        // filteredData.forEach((pair) =>{
-
-        //     Object.entries(pair).forEach(function([key, value]){
-        //         console.log(key, value);
-        //     })
-        // })
+        //Adding data to the body
+        var body = d3.select("#sample-metadata");
+        body.html("");
+        for (let [key, value] of Object.entries(filteredData)) {
+            body.append("p").text(`${key}: ${value}`);
+          }
     }
-   function optionChanged(ID){}
+
+    function plots(ID){
+
+        //Getting the sample Data
+        var sampleData = data.samples;
+
+        var filteredData = [];
+
+        //Filtering out the data for the specific ID
+        for (var i = 0; i < sampleData.length; i++){
+
+            if(sampleData[i].id == ID){
+                filteredData.push(sampleData[i]);
+                break;
+            }
+        };
+
+        console.log(filteredData);
+
+        var otuIds = [];
+        var sampleValues = [];
+        var otuLabels = [];
+        var colorIDs = [];
+
+        //Getting the top 10 otu ids for the chosen ID
+        for(var i = 0; i < 10; i++){
+                otuIds.push(filteredData[0].otu_ids[i]);
+                colorIDs.push(filteredData[0].otu_ids[i]);
+                sampleValues.push(filteredData[0].sample_values[i]);
+                otuLabels.push(filteredData[0].otu_labels[i]);
+
+        };
+
+        //Appending OTU at the start of the ID's
+        for (var j =0; j < otuIds.length; j++){
+            otuIds[j] = 'OTU ' + otuIds[j];
+        };
+
+        //Bar Plot
+        var barData = [{
+            type: 'bar',
+            x: sampleValues.reverse(),
+            y: otuIds.reverse(),
+            orientation: 'h',
+            text: otuLabels.reverse()
+        }];
+
+        var barLayout = {
+            yaxis:{range: otuIds, width: 400},
+        };
+
+        //Adding plot to the html page
+        Plotly.newPlot('bar', barData, barLayout);
+
+        //Bubble Plot
+        var bubbleData = [{
+            type: 'scatter',
+            mode: 'markers',
+            x: colorIDs,
+            y: sampleValues,
+            marker: {
+                size: sampleValues,
+                color: colorIDs
+            },
+            hovertext: otuLabels
+        }];
+
+        //Adding plot to the html page
+        Plotly.newPlot('bubble', bubbleData);
+    }
+
+    //Calling the function to change if the value in drop down menu changes
+   d3.select("select").on("change",function(d){
+       var selected = d3.select("#selDataset").node().value;
+       console.log(selected);
+       getMetaData(selected);
+       plots(selected);
+
+   });
 });
